147a559a83e785b6f9a3213482781ab2447af1bc
Dradwing <Rushbuket@gmail.com>
2022-08-23 14:26:36 +0530

    try3

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -2,52 +2,48 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, { apiVersion: '2
 const Tour = require('../models/tourModel');
 const User = require('../models/userModel');
 const Booking = require('../models/bookingModel');
-//const catchAsync = require('../utils/catchAsync');
+const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
-exports.getCheckoutSession = async (req, res, next) => {
-  try {
-    // 1) Get the currently booked tour
-    const tour = await Tour.findById(req.params.tourId);
-    // console.log(tour);
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  // 1) Get the currently booked tour
+  const tour = await Tour.findById(req.params.tourId);
+  // console.log(tour);
 
-    // 2) Create checkout session
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
+  // 2) Create checkout session
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
 
-      // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
-      //   req.params.tourId
-      // }&user=${req.user.id}&price=${tour.price}`,
+    // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
+    //   req.params.tourId
+    // }&user=${req.user.id}&price=${tour.price}`,
 
-      success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
 
-      cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
 
-      customer_email: req.user.email,
+    customer_email: req.user.email,
 
-      client_reference_id: req.params.tourId,
+    client_reference_id: req.params.tourId,
 
-      line_items: [
-        {
-          name: `${tour.name} Tour`,
-          description: tour.summary,
-          images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
-          amount: tour.price * 100,
-          currency: 'INR',
-          quantity: 1
-        }
-      ]
-    });
+    line_items: [
+      {
+        name: `${tour.name} Tour`,
+        description: tour.summary,
+        images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
+        amount: tour.price * 100,
+        currency: 'INR',
+        quantity: 1
+      }
+    ]
+  });
 
-    // 3) Create session as response
-    res.status(200).json({
-      status: 'success',
-      session
-    });
-  } catch (e) {
-    console.log(e);
-  }
-};
+  // 3) Create session as response
+  res.status(200).json({
+    status: 'success',
+    session
+  });
+});
 
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -4,8 +4,7 @@ import axios from 'axios';
 import { showAlert } from './alerts';
 
 const stripe = Stripe(
-  'pk_test_51LZcsHSCynPOuVgGet5Rz1oebPZCAfIcXFE97tx5435fgYNVRITv8cpGcVjfk4QscSbZwkDufaWtGRfZ7BEyYTig00gFPS6zgE',
-  { apiVersion: '2020-08-27' }
+  'pk_test_51LZcsHSCynPOuVgGet5Rz1oebPZCAfIcXFE97tx5435fgYNVRITv8cpGcVjfk4QscSbZwkDufaWtGRfZ7BEyYTig00gFPS6zgE'
 );
 
 export const bookTour = async tourId => {
